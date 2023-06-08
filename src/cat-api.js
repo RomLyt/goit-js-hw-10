270f9fb98db822a4243be52a0dca1c82e5263623
lytvynenkoroman84@gmail.com <121231829+RomLyt@users.noreply.github.com>
2023-06-08 11:33:07 +0300

    first

---
+++ src/cat-api.js
@@ -0,0 +1,35 @@
+import Notiflix from 'notiflix';
+
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+const API_KEY =
+  'live_rSmz8wcziJ9DKVQVcb1LwFltqDXUplNOjySfVOj6WyBXzMkdZTc7JOszgHDmDj2T';
+const CAT_SEARCH = 'https://api.thecatapi.com/v1/images/search';
+
+export function fetchBreeds() {
+  const params = new URLSearchParams({
+    apikey: API_KEY,
+  });
+  return fetch(`${BASE_URL}?${params}`)
+    .then(a => {
+      if (!a.ok) {
+        throw new error(a.status);
+      }
+      return a.json();
+    })
+    .catch(() =>
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      )
+    );
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(`${CAT_SEARCH}?breed_ids=${breedId}&api_key=${API_KEY}`)
+    .then(a => {
+      if (!a.ok) {
+        throw new error(a.status);
+      }
+      return a.json();
+    })
+    .catch(() => console.log());
+}
